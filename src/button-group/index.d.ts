0664c2f6c42ea342f59439bb1398b2a09e4bc199
huangqi03 <huangqi03@baidu.com>
2016-04-19 15:21:42 +0800

    quick push

--- src/button-group/index.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as module from './module';
-export default class ButtonGroup extends React.Component<module.PropsInterface, module.StateInterface> {
-    static defaultProps: module.Props;
-    state: module.State;
-    constructor(props: any);
-    render(): any;
-}
--- src/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../../../../typings-module/fit-style.d.ts" />
-import 'fit-style';
-import Button from './button';
-import ButtonGroup from './button-group';
-export default Button;
-export { Button, ButtonGroup };
